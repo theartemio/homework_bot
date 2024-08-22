5bd61b3278a883ead6479513108e36b8fbd02274
Artemii Tretiakov <artemii-tretiakov@gmail.com>
2024-08-22 17:54:37 +0300

    check_type_and_keys func added. Docstrings improved. Minor code improvements. Deleted unnecessary files

--- exceptions.py
+++ exceptions.py
@@ -3,46 +3,42 @@ class TokenMissing(Exception):
         self.missing_token = missing_token
 
     def __str__(self):
-        return f'Проблема с переменными окружения (токенами). Обязательная переменная {self.missing_token} не обнаружена! Бот завершает работу'
+        return f'''Проблема с переменными окружения (токенами).
+        Обязательная переменная {self.missing_token} не обнаружена!
+        Бот завершает работу'''
 
-class ApiUnavailable(Exception):
 
+class ApiError(Exception):
     def __init__(self, response_code):
         self.response_code = response_code
 
     def __str__(self):
-        return f'API сервиса Практикум.Домашка недоступен. Код ответа: {self.response_code}'
-    
-class ExpectedKeyNotFoundTwo(Exception):
-    
+        return f'''Ошибка API сервиса Практикум.Домашка.
+        Код ответа: {self.response_code}'''
+
+
+class ExpectedKeyNotFound(Exception):    
     def __init__(self, missing_key, item_name):
         self.missing_key = missing_key
         self.item_name = item_name
-    
-    def __str__(self):
-        return f'Получен некорректный {self.item_name}. В ответе отсутствует ключ {self.missing_key}.'
 
-class ExpectedKeyNotFound(Exception):
-    
-    def __init__(self, missing_key, item_name):
-        self.missing_key = missing_key
-    
     def __str__(self):
-        return f'Получен некорректный ответ. В ответе отсутствует ключ {self.missing_key}.'
+        return f'''Получен некорректный {self.item_name}.
+        В ответе отсутствует ключ {self.missing_key}.'''
+
 
 class HomeworkNameNotFound(Exception):
     def __str__(self):
         return 'Отсутствует ключ Homework name!'
-    
+
 
 class UnexpectedResponseType(Exception):
     def __str__(self):
         return 'Некорректный ответ сервиса!'
 
-class UnexpectedHomeworkStatus(Exception):
-    
+
+class UnexpectedHomeworkStatus(Exception):    
     def __init__(self, verdict):
         self.verdict = verdict
     def __str__(self):
         return f'Некорректный вердикт! Получен вердикт {self.verdict}'
-
--- homework.py
+++ homework.py
@@ -1,25 +1,16 @@
 
-import os
-import requests
-import time
-from dotenv import load_dotenv
-
-from telebot import TeleBot, types
 import logging
+import os
+import sys
+import time
 
-from exceptions import (
-    TokenMissing,
-    UnexpectedResponseType,
-    ExpectedKeyNotFound,
-    UnexpectedHomeworkStatus,
-    ApiUnavailable,
-    HomeworkNameNotFound,
-    ExpectedKeyNotFoundTwo
-)
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+from telebot import TeleBot
 
-
-
-
+from exceptions import (ApiError, ExpectedKeyNotFound, TokenMissing,
+                        UnexpectedHomeworkStatus, UnexpectedResponseType)
 
 load_dotenv()
 
@@ -29,7 +20,6 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-# ENDPOINT = 'http://www.google.com/404'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 HOMEWORK_VERDICTS = {
@@ -38,6 +28,7 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG,
@@ -45,14 +36,18 @@ logging.basicConfig(
     filemode='a'
 )
 
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
 
 def check_tokens():
-    '''
-    Функция проверяет существование обязательных переменных окружения:
+    """
+    Функция проверяет существование обязательных переменных окружения.
+    Проверяемые переменные:
     PRACTICUM_TOKEN
     TELEGRAM_TOKEN
     TELEGRAM_CHAT_ID
-    '''
+    """
     tokens = {'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
               'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
               'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
@@ -61,158 +56,157 @@ def check_tokens():
         if not tokens[token]:
             raise TokenMissing(token)
 
+
 def send_message(bot, message):
-    '''
-    Отправляет сообщение в Telegram-чат, определяемый переменной окружения
-    TELEGRAM_CHAT_ID.
-    Принимает два параметра:
+    """
+    Отправляет сообщение в Telegram-чат.
+    Чат определяется переменной окружения TELEGRAM_CHAT_ID.
+    В качестве параметров функция принимает:
     bot - экземпляр класса TeleBot
     message - строку с текстом сообщения
-    '''
+    """
     chat_id = TELEGRAM_CHAT_ID
     try:
         bot.send_message(chat_id=chat_id, text=message)
-        logging.debug(f'Успешно отправлено сообщение: {message}')
+        logger.debug(f'Успешно отправлено сообщение: {message}')
     except Exception as error:
-        logging.error(error, exc_info=True)
+        logger.error(error, exc_info=True)
 
 
 def get_api_answer(timestamp):
-    '''
-    Получает ответ, содержащий список домашек, статус которых поменялся в период
-    от timestamp до настоящего времени. Принимает аргументы:
+    """
+    Получает ответ API.
+    Ответ должен содержать список домашек, статус
+    которых поменялся в период от timestamp до настоящего времени.
+    В качестве параметров функция принимает:
     timestamp - временную метку в формате Unix-времени
-    '''
+    """
     try:
-        homework_statuses = requests.get(url=ENDPOINT, headers=HEADERS, params=timestamp)
+        homework_statuses = requests.get(url=ENDPOINT,
+                                         headers=HEADERS,
+                                         params=timestamp)
         if homework_statuses.status_code != 200:
-            raise ApiUnavailable(homework_statuses.status_code)
+            raise ApiError(homework_statuses.status_code)
         homework_statuses_json = homework_statuses.json()
         return homework_statuses_json
     except requests.RequestException as error:
-        logging.error(error, exc_info=True)
+        logger.error(error, exc_info=True)
 
-########
 
-def check_keys(response, example_dict, now_checking):
-    '''Проверяет наличие ключей, а также типы их значений на соответствие документации API сервиса Практикум.Домашка.
-    В качестве параметра функция получает:
-    response - словарь, который необходимо проверить
-    example_dict - словарь, с которым необходимо сравнить
-    now_checking - что проверяем'''
+def check_type_and_keys(response, example_element, element_name):
+    """
+    Проверяет наличие ключей и типы их значений.
+    Ключи должны соответствовать документации API сервиса Практикум.Домашка.
+    В качестве параметров функция принимает:
+    response - Элемент, который необходимо проверить
+    example_element - Пример данных, с которыми необходимо сравнить response
+    element_name - Название проверяемого элемента для сообщений об ошибках
+    и логов
+    """
     reponse_type = type(response)
-    expected_type = type(example_dict)
+    expected_type = dict
     if not isinstance(response, expected_type):
-        raise TypeError(f'Некорректный тип {now_checking}. Ожидался {expected_type.__name__}, получен {reponse_type.__name__}.')
-    logging.info(f'Тип {now_checking} проверен: полученный тип {reponse_type} соответствует ожидаемому.')
-    for key, value in example_dict.items():
+        raise TypeError(f'''Некорректный {element_name}.
+                        Ожидался {expected_type.__name__},
+                        получен {reponse_type.__name__}.''')
+    logger.info(f'''Тип {element_name} проверен:
+                 полученный тип {reponse_type}
+                 соответствует ожидаемому.''')
+    for key, value in example_element.items():
         if key not in response.keys():
-            raise ExpectedKeyNotFoundTwo(key, now_checking)
+            raise ExpectedKeyNotFound(key, element_name)
         if not isinstance(response[key], value):
-            raise TypeError(f'Некорректный {now_checking}. Ожидался тип значения {key} равный {value}, получен {type(response[key])}')
-    logging.info(f'{now_checking} проверен. Все ожидаемые ключи {now_checking.keys()} на месте.')
-
-
-#####
+            raise TypeError(f'''Некорректный {element_name}.
+                            Ожидался тип значения {key} равный {value},
+                            получен {type(response[key])}''')
+    logger.info(f'''{element_name} проверен.
+                 Все ожидаемые ключи {example_element.keys()}
+                 на месте.''')
 
 
 def check_response(response):
-    '''Проверяет ответ на соответствие документации API сервиса Практикум.Домашка.
+    """
+    Проверяет ответ API на соответствие документации.
     В качестве параметра функция получает:
-    response - ответ API, приведённый к типам данных Python.'''
-    
-    reponse_type = type(response)
-    expected_type = dict
+    response - ответ API, приведённый к типам данных Python.
+    """
     response_keys_and_types = {'homeworks': list,
                                'current_date': int,
                                }
-    
+    checking = 'Ответ API'
+    check_type_and_keys(response, response_keys_and_types, checking)
+    logger.info(f'''Ответ API проверен.
+                 Все ожидаемые ключи {response_keys_and_types.keys()}
+                 на месте.''')
 
-    
-    if not isinstance(response, expected_type):
-        raise TypeError(f'Некорректный тип ответа API. Ожидался {expected_type.__name__}, получен {reponse_type.__name__}.')
-    logging.info(f'Тип ответа API проверен: полученный тип {reponse_type} соответствует ожидаемому.')
-    for key, value in response_keys_and_types.items():
-        if key not in response.keys():
-            raise ExpectedKeyNotFound(key)
-        if not isinstance(response[key], value):
-            raise TypeError(f'Некорректный ответ API. Ожидался тип значения {key} равный {value}, получен {type(response[key])}')
-    
-    
-    logging.info(f'Ответ API проверен. Все ожидаемые ключи {response_keys_and_types.keys()} на месте.')
 
 def parse_status(homework):
-    '''
-    Извлекает из словаря с информацией о конкретной домашней работе статус этой работы.
-    В качестве параметра функция получает только один элемент из списка домашних работ.
-    В случае успеха функция возвращает подготовленную для отправки в Telegram строку,
-    содержащую один из вердиктов словаря HOMEWORK_VERDICTS
-    '''
-    homework_keys_and_types = {'homework_name': str, 'status': str}
-
-
-    
-    if 'homework_name' not in homework.keys():
-        raise HomeworkNameNotFound
-    homework_keys_and_types = {'homework_name': str, 'status': str}
-    for key, value in homework_keys_and_types.items():
-        if key not in homework.keys():
-            raise ExpectedKeyNotFound(key)
-        if not isinstance(homework[key], value):
-            raise TypeError(f'Некорректный ответ API. Ожидался тип значения {key} равный {value}, получен {type(homework[key])}')
-    
-    
-    homework_name = homework['homework_name']
+    """
+    Извлекает статус домашки.
+    Извлекает из словаря с информацией о конкретной домашней работе
+    статус этой работы.
+    В случае успеха функция возвращает подготовленную для отправки
+    в Telegram строку, содержащую один из вердиктов словаря HOMEWORK_VERDICTS.
+    В качестве параметра функция получает:
+    homework - словарь с информацией о домашней работе,
+    описанный в документации к API
+    """
+    homework_keys_and_types = {'homework_name': str,
+                               'status': str
+                               }
+    checking = 'Элемент из списка с домашкой'
+    check_type_and_keys(homework, homework_keys_and_types, checking)
     homework_status = homework['status']
-    if homework_status in HOMEWORK_VERDICTS.keys():
-        verdict = HOMEWORK_VERDICTS[homework_status]
-        return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-    else:
+    homework_name = homework['homework_name']
+    if homework_status not in HOMEWORK_VERDICTS.keys():
         raise UnexpectedHomeworkStatus(homework_status)
+    verdict = HOMEWORK_VERDICTS[homework_status]
+    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
-    '''
-    Основная логика работы бота.
-    '''
+    """Основная логика работы бота."""
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     timestamp = 0
     while True:
         try:
-            send_message(bot, f'Проверяю период от {timestamp} до {int(time.time())}')
+            send_message(bot, f'''Проверяю период
+                         от {timestamp} до {int(time.time())}''')
+            
             check_tokens()
             payload = {'from_date': timestamp}
             homeworks = get_api_answer(payload)
             check_response(homeworks)
             homeworks_list = homeworks['homeworks']
             if not homeworks_list:
-                logging.debug(f'В ответе отсутствуют обновления статусов домашки (пустой ответ).')
-                send_message(bot, 'Обновлений пока нет')
+                logger.debug('''В ответе отсутствуют обновления статусов
+                              домашки (список работ под ключом
+                              "homeworks" пуст).''')
+                send_message(bot, '''Проверен период от  
+                             Обновлений пока нет''')
             for homework in homeworks_list:
                 try:
                     status_update = parse_status(homework)
                     send_message(bot, status_update)
                 except UnexpectedHomeworkStatus as error:
                     send_message(bot, f'Возникла ошибка! {error}')
-                    logging.error(error, exc_info=True)
+                    logger.error(error, exc_info=True)
                     continue
             timestamp = int(time.time())
             time.sleep(RETRY_PERIOD)
         except TokenMissing as error:
-            logging.critical(error, exc_info=True)
+            logger.critical(error, exc_info=True)
+            send_message(bot, f'Возникла ошибка! {error}')
             break
         except (UnexpectedResponseType,
                 ExpectedKeyNotFound,
                 TypeError,
-                ApiUnavailable) as error:
+                ApiError) as error:
             send_message(bot, f'Возникла ошибка! {error}')
-            logging.error(error, exc_info=True)
+            logger.error(error, exc_info=True)
             time.sleep(RETRY_PERIOD)
             continue
-            
-
-            
 
 
 if __name__ == '__main__':
